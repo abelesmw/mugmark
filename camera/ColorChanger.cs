913f80acd465e3c056831dbd38c0c9b2cd24f796
Mark <abelesmw@gmail.com>
2023-07-28 08:29:13 -0400

    Add files via upload
    lerps from starting color to end color for the fire level

---
+++ camera/ColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private Camera camera;
+    public Transform target;
+    public float colorChangeSpeed = 0.5f;
+    public float endColorPosition;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Color startColor = new Color(0.5f, 0.5f, 0.5f, 1f);
+        Color endColor = Color.black;
+        float t = Mathf.Clamp01(target.position.x / endColorPosition);
+        if(transform.position.x > 46)
+        {
+            camera.backgroundColor = Color.Lerp(startColor, endColor, t * colorChangeSpeed);
+        }
+        
+    }
+}
