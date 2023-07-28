4f34ddd3ccc729e2b9fa88f80e322b4bc4a48cb1
Mark <abelesmw@gmail.com>
2023-07-28 08:23:21 -0400

    Add files via upload

---
+++ environments/cloudBackground.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+    public class cloudBackground: MonoBehaviour
+    {
+
+    public GameObject Cloud;
+    public float cloudTimer;
+    public float timeToNextCloud;
+    System.Random rand = new System.Random();
+    int nextCloudDistance;
+    int nextCloudHeight;
+    int lastCloudPosition;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        cloudTimer += Time.deltaTime;
+
+        if(cloudTimer > timeToNextCloud)
+        {
+            Instantiate(Cloud, new Vector3(lastCloudPosition + nextCloudDistance, nextCloudHeight), Cloud.transform.rotation);
+        }
+    }
+}
+
