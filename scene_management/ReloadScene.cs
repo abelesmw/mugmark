89123f110154a525d0eec476a69b1db342189219
Mark <abelesmw@gmail.com>
2023-07-28 08:22:27 -0400

    Add files via upload

---
+++ scene_management/ReloadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ReloadScene : MonoBehaviour
+{
+    public void Reload()
+    {
+        print("button clicked");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
