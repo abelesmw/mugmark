6106489c7251a5e941dd25966d0bfb66e910d61c
Mark <abelesmw@gmail.com>
2023-07-28 08:16:57 -0400

    Add files via upload

---
+++ scene_management/go_to_stone_level.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class go_to_stone_level : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
