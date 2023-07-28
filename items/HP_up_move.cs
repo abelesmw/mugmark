fbf82638727c33dbc2a8a393ad91291e795100c7
Mark <abelesmw@gmail.com>
2023-07-28 08:12:08 -0400

    Add files via upload

---
+++ items/HP_up_move.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_up_move : MonoBehaviour
+{
+    Transform transformy;
+
+    void Start()
+    {
+        transformy = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transformy.RotateAround(transform.position, transform.up, Time.deltaTime *2 * 90f);
+    }
+
+}
